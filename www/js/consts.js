0d01c62a6b8416cb78e8cba154c720da89b0dc72
vagrant <vagrant@vagrant.vm>
2018-04-04 21:34:43 +0000

    uncomment mistakely commented code

--- www/js/consts.js
+++ www/js/consts.js
@@ -11,11 +11,10 @@ var SCRIPT_AFTER_KEY = "&v=3&callback=initApp\" onerror=\"googleError\"<\\script
 
 /** Stores initial locations. */
 var INITIAL_MARKERS = [
-    {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}}
-    // ,
-    // {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
-    // {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
-    // {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
-    // {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
-    // {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
+    {title: 'Park Ave Penthouse', location: {lat: 40.7713024, lng: -73.9632393}},
+    {title: 'Chelsea Loft', location: {lat: 40.7444883, lng: -73.9949465}},
+    {title: 'Union Square Open Floor Plan', location: {lat: 40.7347062, lng: -73.9895759}},
+    {title: 'East Village Hip Studio', location: {lat: 40.7281777, lng: -73.984377}},
+    {title: 'TriBeCa Artsy Bachelor Pad', location: {lat: 40.7195264, lng: -74.0089934}},
+    {title: 'Chinatown Homey Space', location: {lat: 40.7180628, lng: -73.9961237}}
 ];
